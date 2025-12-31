4ca09e758c0c69b2d17cf5290cef4836d16cde9c
ArthurFloro <arthur.floro2006@gmail.com>
2025-12-30 22:33:19 -0300

    feat: criando a nova tela de criar conta e ajustando seus componentes

---
+++ app/components/auth/signup-form.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+
+export const SignupForm = () => {
+  const router = useRouter(); // para conseguir redirecionar o usuário após o login
+  const [nameField, setNameField] = useState(""); // para salvar o que o usuário digita
+  const [emailField, setEmailField] = useState(""); // para salvar o que o usuário digita no email
+  const [passwordField, setPasswordField] = useState(""); // para salvar o que o usuário digita
+
+  //redireciona o usuário para a página /home apos o login
+  const handleEnterButton = () => {
+    router.replace("/home");
+  };
+
+  return (
+    <>
+      <Input
+        placeholder="Digite seu nome"
+        value={nameField}
+        onChange={(t) => setNameField(t)}
+      />
+
+      <Input
+        placeholder="Digite seu e-mail"
+        value={emailField}
+        onChange={(t) => setEmailField(t)}
+      />
+
+      <Input
+        placeholder="Digite sua senha"
+        value={passwordField}
+        onChange={(t) => setPasswordField(t)}
+        password
+      />
+
+      <Button label="Criar conta" onClick={handleEnterButton} size={1} />
+    </>
+  );
+};
