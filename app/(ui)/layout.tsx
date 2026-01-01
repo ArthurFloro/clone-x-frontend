1fa6e6c31030bf03aa3ed961408877faec60762c
ArthurFloro <arthur.floro2006@gmail.com>
2026-01-01 14:49:57 -0300

    feat: componente nav my profile, preprando o front para receber dados do backend

--- app/(ui)/layout.tsx
+++ app/(ui)/layout.tsx
@@ -3,6 +3,7 @@ import { Logo } from "../components/ui/logo";
 import { NavItem } from "../components/nav/nav-item";
 import { faHouse, faUser } from "@fortawesome/free-solid-svg-icons";
 import { NavLogout } from "../components/nav/nav-logout";
+import { NavMyProfile } from "../components/nav/nav-myprofile";
 
 type Props = {
   children: React.ReactNode;
@@ -11,7 +12,7 @@ type Props = {
 export default function Layout({ children }: Props) {
   return (
     <main className="min-h-screen flex justify-center mx-auto max-w-7xl">
-      <section className="flex lg:flex flex-col sticky top-0 h-screen w-72 px-3 border-r-2 border-gray-900">
+      <section className="hidden lg:flex flex-col sticky top-0 h-screen w-72 px-3 border-r-2 border-gray-900">
         <div className="flex-1 mt-6">
           <Logo size={24} />
           <nav className="mt-11">
@@ -22,6 +23,7 @@ export default function Layout({ children }: Props) {
         </div>
         <div className="mb-6 flex flex-col gap-4">
           <NavLogout />
+          <NavMyProfile />
         </div>
       </section>
       <section className="flex-1 max-w-lg">{children}</section>
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ui-avatars.com",
+        port: "",
+        pathname: "/api/**", // Permite caminhos que começam com /api/
+      },
+    ],
+  },
 };
 
 export default nextConfig;
