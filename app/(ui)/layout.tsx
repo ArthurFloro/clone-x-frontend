c2698e853a089643826bc79858624e57d0c8a5da
ArthurFloro <arthur.floro2006@gmail.com>
2026-01-01 15:52:12 -0300

    feat: componente trending area e trending items, uso da propriedade group do tailwind

--- app/(ui)/layout.tsx
+++ app/(ui)/layout.tsx
@@ -5,6 +5,7 @@ import { faHouse, faUser } from "@fortawesome/free-solid-svg-icons";
 import { NavLogout } from "../components/nav/nav-logout";
 import { NavMyProfile } from "../components/nav/nav-myprofile";
 import { SearchInput } from "../components/ui/search-input";
+import { TrendinArea } from "../components/ui/trending-area";
 
 type Props = {
   children: React.ReactNode;
@@ -30,6 +31,7 @@ export default function Layout({ children }: Props) {
       <section className="flex-1 max-w-lg">{children}</section>
       <aside className="flex lg:flex flex-col gap-6 sticky top-0 h-fit w-96 px-8 py-6 border-l-2 border-gray-900">
         <SearchInput hideOnSearch />
+        <TrendinArea/>
       </aside>
     </main>
   );
