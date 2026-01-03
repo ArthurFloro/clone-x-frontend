a4b353d9abb7d4939aae4d1f06de98ec9de9c651
ArthurFloro <arthur.floro2006@gmail.com>
2026-01-03 00:44:46 -0300

    feat: component comments

--- app/components/tweet/tweet-item.tsx
+++ app/components/tweet/tweet-item.tsx
@@ -15,9 +15,10 @@ import { useState } from "react";
 
 type Props = {
   tweet: Tweet;
+  hideComments?: boolean;
 };
 
-export const TweetItem = ({ tweet }: Props) => {
+export const TweetItem = ({ tweet, hideComments }: Props) => {
   const [liked, setLiked] = useState(tweet.liked);
 
   const handleLikeButton = () => {
@@ -60,14 +61,16 @@ export const TweetItem = ({ tweet }: Props) => {
           </div>
         )}
         <div className="flex mt-6 text-gray-500">
-          <div className="flex-1">
-            <Link href={`/tweet/${tweet.id}`}>
-              <div className="inline-flex items-center gap-2 cursor-pointer">
-                <FontAwesomeIcon icon={faComment} size="lg" />
-                <div className="text-lg">{tweet.commentCount}</div>
-              </div>
-            </Link>
-          </div>
+          {!hideComments && (
+            <div className="flex-1">
+              <Link href={`/tweet/${tweet.id}`}>
+                <div className="inline-flex items-center gap-2 cursor-pointer">
+                  <FontAwesomeIcon icon={faComment} size="lg" />
+                  <div className="text-lg">{tweet.commentCount}</div>
+                </div>
+              </Link>
+            </div>
+          )}
           <div className="flex-1">
             <div className="inline-flex items-center gap-2 cursor-pointer">
               <FontAwesomeIcon icon={faRetweet} size="lg" />
